5650127149a9d0b7fa4c0f6e572710d4646eb5b5
Farhana Ghayas <farhana.capricon@gmail.com>
2025-08-20 12:38:29 +0200

    feat: reuse single modal for both add and edit task functionality

--- client/src/app/pages/dashboard/dashboard.ts
+++ client/src/app/pages/dashboard/dashboard.ts
@@ -5,7 +5,7 @@ import {  RouterLink } from '@angular/router';
 import { TaskService } from '../../services/taskservice';
 import { TaskItem } from '../../interfaces/task-item';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import * as bootstrap from 'bootstrap';
 
 @Component({
   standalone: true,
@@ -16,7 +16,8 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 
 export class DashboardPage implements OnInit{
 
-
+   modalMode : 'add' | 'edit' = 'add';
+   currentTaskId: number | null = null;
    userName = localStorage.getItem('username');
    taskform : FormGroup;
 
@@ -25,21 +26,68 @@ export class DashboardPage implements OnInit{
       title: ['', Validators.required],
       description : ['', Validators.required]
     });
-    console.log(auth);
+   // console.log(auth);
   }
 
   tasks: TaskItem[] = [];
  
   ngOnInit(){
-   console.log('Dashboard Initialize');
+   //console.log('Dashboard Initialize');
     this.getTasks();
   }
 
   onLogout() {
     this.auth.logout()
   }
-   onAddTask() {
-    console.log('Clicked ');
+  onSubmitTask(){
+     if (this.taskform.invalid) return;
+
+  if (this.modalMode == 'edit' && this.currentTaskId) {
+    // Update existing task
+       this.taskservice.updateTask(this.currentTaskId, this.taskform.value).subscribe(() => {
+      console.log('Task updated');
+      this.getTasks();
+      this.closeModal();
+    });
+  } else {
+    // Add new task
+      this.addTask();
+      this.closeModal();
+    
+  }
+  }
+  closeModal() {
+    const modalElement = document.getElementById('taskModal');
+  if (modalElement) {
+    const modalInstance =bootstrap.Modal.getInstance(modalElement) || new bootstrap.Modal(modalElement);
+    modalInstance.hide();
+  }
+  }
+  openAddModal() {
+  this.modalMode = 'add';
+  this.taskform.reset();
+  this.currentTaskId = null;
+  this.showModal();
+}
+openEditModal(task: TaskItem) {
+  this.modalMode = 'edit';
+  this.currentTaskId = task.id;
+  this.taskform.patchValue({
+    title: task.title,
+    description: task.description
+  });
+  this.showModal();
+}
+showModal() {
+  const modalEl = document.getElementById('taskModal');
+  //console.log(modalEl);
+  if (modalEl) {
+    const modal = new bootstrap.Modal(modalEl);
+    modal.show();
+  }
+}
+   addTask() {
+   // console.log('Clicked ');
     if (this.taskform.invalid) return;
 
     this.taskservice.createTask(this.taskform.value).subscribe(() => {
@@ -54,7 +102,7 @@ export class DashboardPage implements OnInit{
     this.taskservice.dltTask(id).subscribe({
       next: () => {
         alert('✅ Task deleted successfully');
-        this.tasks = this.tasks.filter(task => task.id !== id); // update list without refresh
+      this.getTasks();
       },
       error: (err) => {
         alert('❌ Failed to delete task');
@@ -65,6 +113,7 @@ export class DashboardPage implements OnInit{
 
 
   }
+
    getTasks() {
       this.taskservice.getTasks().subscribe({
             next: (data) => {
